c78ad95dfb029ff79317d55e3991e4420c087b57
UdayBaskar8985 <udaybaskar8985@gmail.com>
2024-01-22 20:28:03 +0530

    Updated string maipulation file

---
+++ string_manipulation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Concatenate strings
+first_name="John"
+last_name="Doe"
+full_name="$first_name $last_name"
+echo "Full Name: $full_name"
+
+# String length
+echo "Length of first name: ${#first_name}"
+
